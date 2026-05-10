f482ed548d098990c88addc8b1750709adf86496
Valentina Modyrka <valentinamodyrka1@gmail.com>
2026-05-10 15:36:31 -0600

    fixed

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,31 +1,24 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { api } from "../../api";
 import { parse } from "cookie";
 import { isAxiosError } from "axios";
-
-import { api } from "../../api";
 import { logErrorResponse } from "../../_utils/utils";
 
 export async function GET() {
   try {
     const cookieStore = await cookies();
-
     const accessToken = cookieStore.get("accessToken")?.value;
     const refreshToken = cookieStore.get("refreshToken")?.value;
 
     if (accessToken) {
-      return NextResponse.json({ success: true }, { status: 200 });
+      return NextResponse.json({ success: true });
     }
 
     if (refreshToken) {
-      const cookiesString = cookieStore
-        .getAll()
-        .map(({ name, value }) => `${name}=${value}`)
-        .join("; ");
-
       const apiRes = await api.get("auth/session", {
         headers: {
-          Cookie: cookiesString,
+          Cookie: cookieStore.toString(),
         },
       });
 
@@ -33,49 +26,30 @@ export async function GET() {
 
       if (setCookie) {
         const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-
         for (const cookieStr of cookieArray) {
           const parsed = parse(cookieStr);
 
           const options = {
             expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
             path: parsed.Path,
-            maxAge: parsed["Max-Age"] ? Number(parsed["Max-Age"]) : undefined,
+            maxAge: Number(parsed["Max-Age"]),
           };
 
-          if (parsed.accessToken) {
+          if (parsed.accessToken)
             cookieStore.set("accessToken", parsed.accessToken, options);
-          }
-
-          if (parsed.refreshToken) {
+          if (parsed.refreshToken)
             cookieStore.set("refreshToken", parsed.refreshToken, options);
-          }
         }
-
         return NextResponse.json({ success: true }, { status: 200 });
       }
     }
-
-    return NextResponse.json({ success: false }, { status: 401 });
+    return NextResponse.json({ success: false }, { status: 200 });
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
-
-      return NextResponse.json(
-        {
-          success: false,
-          error: error.message,
-          response: error.response?.data,
-        },
-        { status: error.status }
-      );
+      return NextResponse.json({ success: false }, { status: 200 });
     }
-
     logErrorResponse({ message: (error as Error).message });
-
-    return NextResponse.json(
-      { success: false, error: "Internal Server Error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ success: false }, { status: 200 });
   }
 }
